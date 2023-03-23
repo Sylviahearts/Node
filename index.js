e82d48308bb1df93b2d4acb118dda8e0f1137caf
Sylvia <114693641+Sylviahearts@users.noreply.github.com>
2023-03-23 14:29:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let name = "Sylvia osimiri" 
+let favLang = "Javascript"
+let futureGoal = "Full-stack Developer"
+
+console.log("My name is " + name + ". My favorite programming language is " + favLang + ". In the next 1 year i see myself as a "  + futureGoal)
